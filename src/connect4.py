5a0375592934f4e4256e2dfb6eec5c0e0c11ad32
Joaquim Del Monte <joaquimdelmonte@gmail.com>
2024-05-21 00:16:43 +0300

    Update connect4.py

--- src/connect4.py
+++ src/connect4.py
@@ -41,30 +41,30 @@ def winning_move(board, piece):
     # Check horizontal locations for win
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT):
-            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
-                c + 3] == piece:
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
                 return True
 
     # Check vertical locations for win
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT - 3):
-            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
-                c] == piece:
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
                 return True
 
-    # Check positively sloped diaganols
+    # Check positively sloped diagonals
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT - 3):
-            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
-                c + 3] == piece:
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
                 return True
 
-    # Check negatively sloped diaganols
+    # Check negatively sloped diagonals
     for c in range(COLUMN_COUNT - 3):
         for r in range(3, ROW_COUNT):
-            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
-                c + 3] == piece:
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
                 return True
+
+
+def is_draw(board):
+    return not np.any(board == 0)
 
 
 def draw_board(board):
@@ -80,10 +80,17 @@ def draw_board(board):
                 pygame.draw.circle(screen, RED, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
                 pygame.draw.circle(screen, BLACK, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS, 5)
             elif board[r][c] == 2:
-                # Dessiner un pion jaune avec une bordure noire
                 pygame.draw.circle(screen, YELLOW, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
                 pygame.draw.circle(screen, BLACK, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS, 5)
     pygame.display.update()
+
+
+def reset_game():
+    global board, game_over, turn
+    board = create_board()
+    game_over = False
+    turn = 0
+    draw_board(board)
 
 
 board = create_board()
@@ -108,11 +115,12 @@ pygame.display.update()
 
 myfont = pygame.font.SysFont("monospace", 75)
 
-while not game_over:
+while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
 
         if event.type == pygame.MOUSEMOTION:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
@@ -121,11 +129,10 @@ while not game_over:
                 pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
             else:
                 pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
-        pygame.display.update()
+            pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-            # print(event.pos)
             # Ask for Player 1 Input
             if turn == 0:
                 posx = event.pos[0]
@@ -140,8 +147,7 @@ while not game_over:
                         screen.blit(label, (40, 10))
                         game_over = True
 
-
-            # # Ask for Player 2 Input
+            # Ask for Player 2 Input
             else:
                 posx = event.pos[0]
                 col = int(math.floor(posx / SQUARESIZE))
@@ -158,9 +164,15 @@ while not game_over:
             print_board(board)
             draw_board(board)
 
+            if is_draw(board) and not game_over:
+                label = myfont.render("Draw", 1, BLUE)
+                screen.blit(label, (40, 10))
+                game_over = True
+
+            if game_over:
+                pygame.time.wait(3000)
+                reset_game()
+
             turn += 1
             turn = turn % 2
 
-            if game_over:
-                pygame.time.wait(1000)
-                pygame.quit()
